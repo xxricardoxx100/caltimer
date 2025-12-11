5b75133892000016836df6187b562e105fb41d70
Ricardo <xxricardoxx7779@gmail.com>
2025-12-11 15:53:44 -0500

    actualizacion subastas

--- src/app/components/subastas/SubastaData.js
+++ src/app/components/subastas/SubastaData.js
@@ -26,7 +26,7 @@ export const subastaData = [
     color: "Anaranjado",
     puertas: 4,
     // Fecha de finalización de la subasta (formato ISO 8601)
-    fecha_fin: "2025-12-11T15:55:00",
+    fecha_fin: "2025-12-11T16:05:00",
     anexoUrl: "/anexo/TyC.pdf",
     descripcion:  [
       "<strong>Nota importante:</strong> el comprador acepta que el vehículo podría tener papeletas, multas o cargas desconocidas por el vendedor al momento de la venta, y asume toda responsabilidad por cualquier afectación que aparezca después de la transferencia.",
@@ -76,7 +76,7 @@ export const subastaData = [
     color: "Negro",
     puertas: 4,
     // Fecha de finalización de la subasta (formato ISO 8601)
-    fecha_fin: "2025-12-11T16:10:00",
+    fecha_fin: "2025-12-11T16:20:00",
     anexoUrl: "/anexo/TyC.pdf",
     descripcion:  [
       "<strong>Nota importante:</strong> el comprador acepta que el vehículo podría tener papeletas, multas o cargas desconocidas por el vendedor al momento de la venta, y asume toda responsabilidad por cualquier afectación que aparezca después de la transferencia.",
