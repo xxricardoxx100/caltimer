fcad390bff1710b8616d337893c516b21267c318
Ricardo <xxricardoxx7779@gmail.com>
2026-04-22 18:43:36 -0500

    nueva subastas

--- src/app/components/subastas/SubastaData.js
+++ src/app/components/subastas/SubastaData.js
@@ -819,7 +819,7 @@ export const subastaData = [
       "Transmision mecanica ",
       "Frenos ABS"
     ]
-  },
+  },//16
   
   {
     id: 17,
@@ -874,7 +874,7 @@ export const subastaData = [
       "Transmision mecanica ",
       "Frenos ABS"
     ]
-  },
+  }, //17
 
   {
     id: 18,
@@ -915,9 +915,111 @@ export const subastaData = [
     ].join("\n"),
     caracteristicas: [
     ]
-  },
+  }, //18
   
+  {
+    id: 19,
+    marca: "Chevrolet",
+    modelo: "Sail",
+    año: 2016,
+    precio: 1600,
+    incremento_puja: 50, 
+    imagen: "/Autos/Chevrolet/AUB135/1.png",
+    imagenes: [
+      "/Autos/Chevrolet/AUB135/1.png",
+      "/Autos/Chevrolet/AUB135/2.png",
+      "/Autos/Chevrolet/AUB135/3.png",
+      "/Autos/Chevrolet/AUB135/4.png",
+      "/Autos/Chevrolet/AUB135/5.png",
+      "/Autos/Chevrolet/AUB135/6.png",
+      "/Autos/Chevrolet/AUB135/7.png",
+      "/Autos/Chevrolet/AUB135/8.png",
+      "/Autos/Chevrolet/AUB135/9.png",
+    ],  
+    kilometraje: "72,900 km",  
+    estado: "Bueno",
+    transmision: "Mecanica",
+    combustible: "BIcombustible GNV",
+    color: "Negro",
+    puertas: 5,
+    // Fecha de finalización de la subasta (formato ISO 8601)
+    fecha_fin: "2026-04-24T18:00:00",
+    anexoUrl: "/anexo/TyC.pdf",
+    boletaInformativaUrl: "/Autos/Chevrolet/AUB135/AUB-135.pdf",
+    descripcion:  [
+      "<strong>PLACA: AUB135</strong>",
+      "<strong>Nota importante:</strong> El comprador acepta que el vehículo podría tener papeletas, multas o cargas desconocidas por el vendedor al momento de la venta, y asume toda responsabilidad por cualquier afectación que aparezca después de la transferencia.",
+      "<strong>UBICACION: El vehiculo se encuentra en  SECPIRV PUNO, PNP PUNO</strong>",
+      "",
+      "La venta de los vehículos se realiza en la modalidad de \"donde está y como está\".",
+      "Los datos y fotografías son referenciales.",
+      "Es responsabilidad del comprador verificar que el número de serie y número de motor correspondan a la unidad.",
+      "El trámite de cambio de placas y entrega del SOAT vigente no está a cargo de la compañía, SI CORRESPONDIERA.",
+      "Todos los gastos relacionados a la logística, transporte o cualquier otro, serán pagados por el comprador y deben ser cancelados antes de la entrega del vehículo.",
+      "",
+      "El precio no incluye IGV. Debe agregar 5% de comisión al precio mostrado.",
+      "Para poder participar en la subasta, el comprador debe haber depositado su garantia de s/300.00 en la cuenta bancaria indicada por la empresa."
+    ].join("\n"),
+    caracteristicas: [
+      "Sistema de sonido premium",
+      "Asientos de Cuero",
+      "Pantalla Digital",
+      "Aros de Serie", 
+      "Gasolina - GLP",
+      "Transmision mecanica ",
+      "Frenos ABS"
+    ]
+  },//19
  
+  {
+    id: 20,
+    marca: "VolskWagen",
+    modelo: "Worker",
+    año: 2004,
+    precio: 22000,
+    incremento_puja: 50, 
+    imagen: "/Autos/Volkswagen/BSG794/1.png",
+    imagenes: [
+      "/Autos/Volkswagen/BSG794/1.png",
+      "/Autos/Volkswagen/BSG794/2.png",
+      "/Autos/Volkswagen/BSG794/3.png",
+      "/Autos/Volkswagen/BSG794/4.png",
+      "/Autos/Volkswagen/BSG794/5.png",
+    ],  
+    kilometraje: "000,000 km",  
+    estado: "Bueno",
+    transmision: "Mecanica",
+    combustible: "Diesel",
+    color: "Blanco",
+    puertas: 5,
+    // Fecha de finalización de la subasta (formato ISO 8601)
+    fecha_fin: "2026-04-24T18:20:00",
+    anexoUrl: "/anexo/TyC.pdf",
+    boletaInformativaUrl: "/Autos/Volkswagen/BSG794/BSG-794.pdf",
+    descripcion:  [
+      "<strong>PLACA: BSG794</strong>",
+      "<strong>Nota importante:</strong> El comprador acepta que el vehículo podría tener papeletas, multas o cargas desconocidas por el vendedor al momento de la venta, y asume toda responsabilidad por cualquier afectación que aparezca después de la transferencia.",
+      "<strong>UBICACION: El vehiculo se encuentra en  Lima</strong>",
+      "",
+      "La venta de los vehículos se realiza en la modalidad de \"donde está y como está\".",
+      "Los datos y fotografías son referenciales.",
+      "Es responsabilidad del comprador verificar que el número de serie y número de motor correspondan a la unidad.",
+      "El trámite de cambio de placas y entrega del SOAT vigente no está a cargo de la compañía, SI CORRESPONDIERA.",
+      "Todos los gastos relacionados a la logística, transporte o cualquier otro, serán pagados por el comprador y deben ser cancelados antes de la entrega del vehículo.",
+      "",
+      "El precio no incluye IGV. Debe agregar 5% de comisión al precio mostrado.",
+      "Para poder participar en la subasta, el comprador debe haber depositado su garantia de s/300.00 en la cuenta bancaria indicada por la empresa."
+    ].join("\n"),
+    caracteristicas: [
+      "Sistema de sonido premium",
+      "Asientos de Cuero",
+      "Pantalla Digital",
+      "Aros de Serie", 
+      "Gasolina - GLP",
+      "Transmision mecanica ",
+      "Frenos ABS"
+    ]
+  },//20
 ];
 
 
