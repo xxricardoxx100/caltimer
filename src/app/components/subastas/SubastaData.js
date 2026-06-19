6fceb5042cff2e24bda92c05ce6e435c7ed3d3f0
Ricardo <xxricardoxx7779@gmail.com>
2026-06-19 17:49:20 -0500

    nuevo veijculo

--- src/app/components/subastas/SubastaData.js
+++ src/app/components/subastas/SubastaData.js
@@ -1454,5 +1454,58 @@ export const subastaData = [
       "Frenos ABS"
     ]
   },//33 kia sorento
+
+  { 
+    id: 34,
+    marca: "Renault",
+    modelo: "Logam",
+    año: 2022,
+    precio: 5500,
+    incremento_puja: 50, 
+    imagen: "/Autos/Renault/harry/1.png",
+    imagenes: [
+      "/Autos/Renault/harry/1.png",
+      "/Autos/Renault/harry/2.png",
+      "/Autos/Renault/harry/3.png",
+      "/Autos/Renault/harry/4.png",
+      "/Autos/Renault/harry/5.png",
+      "/Autos/Renault/harry/6.png",
+      "/Autos/Renault/harry/7.png",
+    ],  
+    kilometraje: "95,000 km",  
+    estado: "Bueno",
+    transmision: "Mecanica",
+    combustible: "Gasolina",
+    color: "Gris",
+    puertas: 5,
+    // Fecha de finalización de la subasta (formato ISO 8601)
+    fecha_fin: "2026-06-19T18:45:00",
+    anexoUrl: "/anexo/TyC.pdf",
+    boletaInformativaUrl: "/Autos/Renault/harry/BNO-527.pdf",
+    descripcion:  [
+      "<strong>PLACA: </strong>",
+      "<strong>Nota importante:</strong> El comprador acepta que el vehículo podría tener papeletas, multas o cargas desconocidas por el vendedor al momento de la venta, y asume toda responsabilidad por cualquier afectación que aparezca después de la transferencia.",
+      "<strong>UBICACION: El vehiculo se encuentra en Lima</strong>",
+      "",
+      "La venta de los vehículos se realiza en la modalidad de \"donde está y como está\".",
+      "Los datos y fotografías son referenciales.",
+      "Es responsabilidad del comprador verificar que el número de serie y número de motor correspondan a la unidad.",
+      "El trámite de cambio de placas y entrega del SOAT vigente no está a cargo de la compañía, SI CORRESPONDIERA.",
+      "Todos los gastos relacionados a la logística, transporte o cualquier otro, serán pagados por el comprador y deben ser cancelados antes de la entrega del vehículo.",
+      "",
+      "El precio no incluye IGV. Debe agregar 5% de comisión al precio mostrado.",
+    ].join("\n"),
+    caracteristicas: [
+      "Sistema de sonido premium",
+      "Asientos de Cuero",
+      "Pantalla Digital",
+      "Version full 2020 ", 
+      "sunroof panoramico",
+      "4x4 motor v6 3.5",
+      "Gasolina",
+      "Transmision mecanica ",
+      "Frenos ABS"
+    ]
+  },//33 renault harry
 ];
 
