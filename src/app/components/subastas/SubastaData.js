34edf845ef8716366bc177b2d1c757c3788a4f79
Ricardo <xxricardoxx7779@gmail.com>
2026-07-24 12:18:38 -0500

    MEJORA SUBASTA BIERNS

--- src/app/components/subastas/SubastaData.js
+++ src/app/components/subastas/SubastaData.js
@@ -2214,7 +2214,7 @@ export const subastaData = [
       "/Autos/taller/14.jpeg",
 
     ],  
-    kilometraje: "161,000 km",  
+    kilometraje: "000,000 km",  
     estado: "Bueno",
     transmision: "Mecanica",
     combustible: "gasolina",
@@ -2247,6 +2247,103 @@ export const subastaData = [
       "Frenos ABS"
     ]
   },//50 chevrolet n300
+
+  {
+    id: 51,
+    marca: "Yamaha",
+    modelo: "T110",
+    año: 2012,
+    precio: 200,
+    incremento_puja: 50, 
+    imagen: "/Autos/taller/15.jpeg",
+    imagenes: [
+      "/Autos/taller/15.jpeg",
+      "/Autos/taller/16.jpeg",
+      "/Autos/taller/17.jpeg",
+
+    ],  
+    kilometraje: "000,000 km",  
+    estado: "Bueno",
+    transmision: "Mecanica",
+    combustible: "gasolina",
+    color: "negro",
+    puertas: 5,
+    // Fecha de finalización de la subasta (formato ISO 8601)
+    fecha_fin: "2026-07-24T20:00:00",
+    anexoUrl: "/anexo/TyC.pdf",
+    boletaInformativaUrl: "/Autos/taller/C50-913.pdf",
+    descripcion:  [
+      "<strong>PLACA: C50913</strong>",
+      "<strong>Nota importante:</strong> El comprador acepta que el vehículo podría tener papeletas, multas o cargas desconocidas por el vendedor al momento de la venta, y asume toda responsabilidad por cualquier afectación que aparezca después de la transferencia.",
+      "<strong>UBICACION: El vehiculo se encuentra en  Lima</strong>",
+      "",
+      "La venta de los vehículos se realiza en la modalidad de \"donde está y como está\".",
+      "Los datos y fotografías son referenciales.",
+      "Es responsabilidad del comprador verificar que el número de serie y número de motor correspondan a la unidad.",
+      "El trámite de cambio de placas y entrega del SOAT vigente no está a cargo de la compañía, SI CORRESPONDIERA.",
+      "Todos los gastos relacionados a la logística, transporte o cualquier otro, serán pagados por el comprador y deben ser cancelados antes de la entrega del vehículo.",
+      "",
+      "El precio no incluye IGV. Debe agregar 5% de comisión al precio mostrado.",
+    ].join("\n"),
+    caracteristicas: [
+      "Sistema de sonido premium",
+      "Asientos de Cuero",
+      "Pantalla Digital",
+      "Aros de Serie", 
+      "Gasolina ",
+      "Transmision mecanica ",
+      "Frenos ABS"
+    ]
+  },//51 YAMAHAT110 TALLER
+
+  {
+    id: 52,
+    marca: "Kia",
+    modelo: "Cerato",
+    año: 2017,
+    precio: 6000,
+    incremento_puja: 50, 
+    imagen: "/Autos/taller/20.jpeg",
+    imagenes: [
+      "/Autos/taller/18.jpeg",
+      "/Autos/taller/19.jpeg",
+      "/Autos/taller/20.jpeg",
+      "/Autos/taller/21.jpeg",
+
+    ],  
+    kilometraje: "000,000 km",  
+    estado: "Bueno",
+    transmision: "Mecanica",
+    combustible: "gasolina",
+    color: "rojo",
+    puertas: 5,
+    // Fecha de finalización de la subasta (formato ISO 8601)
+    fecha_fin: "2026-07-24T20:00:00",
+    anexoUrl: "/anexo/TyC.pdf",
+    boletaInformativaUrl: "/Autos/taller/AUX-418.pdf",
+    descripcion:  [
+      "<strong>PLACA: AUX418</strong>",
+      "<strong>Nota importante:</strong> El comprador acepta que el vehículo podría tener papeletas, multas o cargas desconocidas por el vendedor al momento de la venta, y asume toda responsabilidad por cualquier afectación que aparezca después de la transferencia.",
+      "<strong>UBICACION: El vehiculo se encuentra en  Lima</strong>",
+      "",
+      "La venta de los vehículos se realiza en la modalidad de \"donde está y como está\".",
+      "Los datos y fotografías son referenciales.",
+      "Es responsabilidad del comprador verificar que el número de serie y número de motor correspondan a la unidad.",
+      "El trámite de cambio de placas y entrega del SOAT vigente no está a cargo de la compañía, SI CORRESPONDIERA.",
+      "Todos los gastos relacionados a la logística, transporte o cualquier otro, serán pagados por el comprador y deben ser cancelados antes de la entrega del vehículo.",
+      "",
+      "El precio no incluye IGV. Debe agregar 5% de comisión al precio mostrado.",
+    ].join("\n"),
+    caracteristicas: [
+      "Sistema de sonido premium",
+      "Asientos de Cuero",
+      "Pantalla Digital",
+      "Aros de Serie", 
+      "Gasolina ",
+      "Transmision mecanica ",
+      "Frenos ABS"
+    ]
+  },//52 KIA CERATO TALLER
 
 
 
