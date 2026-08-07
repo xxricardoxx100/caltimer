bc5ca8041f37ea5257569cadf2f7689a43351d6b
Ricardo <xxricardoxx7779@gmail.com>
2026-08-07 09:27:11 -0500

    carro  nuevo

--- src/app/components/subastas/SubastaData.js
+++ src/app/components/subastas/SubastaData.js
@@ -2494,7 +2494,55 @@ export const subastaData = [
     ]
   },//56 accent diego
 
-
+  {
+    id: 57,
+    marca: "Kia",
+    modelo: "Picanto",
+    año: 2015,
+    precio: 3500,
+    incremento_puja: 50, 
+    imagen: "/Autos/taller/kia/1.png",
+    imagenes: [
+      "/Autos/taller/kia/1.png",
+      "/Autos/taller/kia/2.png",
+      "/Autos/taller/kia/3.png",
+      "/Autos/taller/kia/4.png",
+      "/Autos/taller/kia/5.png",
+    ],  
+    kilometraje: "000,000 km",  
+    estado: "Bueno",
+    transmision: "Mecanico",
+    combustible: "Gasolina",
+    color: "Marron",
+    puertas: 4,
+    // Fecha de finalización de la subasta (formato ISO 8601)
+    fecha_fin: "2026-08-07T20:10:00",
+    anexoUrl: "/anexo/TyC.pdf",
+    boletaInformativaUrl: "/Autos/taller/kia/U1D-286.pdf",
+    descripcion:  [
+      "<strong>PLACA: U1D286</strong>",
+      "<strong>Nota importante:</strong> El comprador acepta que el vehículo podría tener papeletas, multas o cargas desconocidas por el vendedor al momento de la venta, y asume toda responsabilidad por cualquier afectación que aparezca después de la transferencia.",
+      "<strong>UBICACION: El vehiculo se encuentra en Lima.</strong>",
+      "",
+      "La venta de los vehículos se realiza en la modalidad de \"donde está y como está\".",
+      "Los datos y fotografías son referenciales.",
+      "Es responsabilidad del comprador verificar que el número de serie y número de motor correspondan a la unidad.",
+      "El trámite de cambio de placas y entrega del SOAT vigente no está a cargo de la compañía, SI CORRESPONDIERA.",
+      "Todos los gastos relacionados a la logística, transporte o cualquier otro, serán pagados por el comprador y deben ser cancelados antes de la entrega del vehículo.",
+      "",
+      "El precio no incluye IGV. Debe agregar 5% de comisión al precio mostrado.",
+      
+    ].join("\n"),
+    caracteristicas: [
+      "Sistema de sonido premium",
+      "Asientos de Tela",
+      "Pantalla Digital",
+      "Aros de Serie", 
+      "Gasolina",
+      "Transmision mecanica",
+      "Frenos ABS"
+    ]
+  },//57 kia taller
 
 
 ];
