b1f890b2411d16428c0addf059133f7a86e891f9
Ricardo <xxricardoxx7779@gmail.com>
2026-08-13 15:19:54 -0500

    carro subasta

--- src/app/components/subastas/SubastaData.js
+++ src/app/components/subastas/SubastaData.js
@@ -2544,6 +2544,57 @@ export const subastaData = [
     ]
   },//57 kia taller
 
+  {
+    id: 58,
+    marca: "Ford",
+    modelo: "Territory",
+    año: 2026,
+    precio: 13000,
+    incremento_puja: 50, 
+    imagen: "/Autos/taller/ford/1.png",
+    imagenes: [
+      "/Autos/taller/ford/1.png",
+      "/Autos/taller/ford/2.png",
+      "/Autos/taller/ford/3.png",
+      "/Autos/taller/ford/4.png",
+      "/Autos/taller/ford/5.png",
+      "/Autos/taller/ford/6.png",
+    ],  
+    kilometraje: "000,000 km",  
+    estado: "Bueno",
+    transmision: "Automatico",
+    combustible: "Gasolina",
+    color: "Blanco",
+    puertas: 4,
+    // Fecha de finalización de la subasta (formato ISO 8601)
+    fecha_fin: "2026-08-14T19:10:00",
+    anexoUrl: "/anexo/TyC.pdf",
+    boletaInformativaUrl: "/Autos/taller/ford/CVH-381.pdf",
+    descripcion:  [
+      "<strong>PLACA: CVH381</strong>",
+      "<strong>Nota importante:</strong> El comprador acepta que el vehículo podría tener papeletas, multas o cargas desconocidas por el vendedor al momento de la venta, y asume toda responsabilidad por cualquier afectación que aparezca después de la transferencia.",
+      "<strong>UBICACION: El vehiculo se encuentra en Lima.</strong>",
+      "",
+      "La venta de los vehículos se realiza en la modalidad de \"donde está y como está\".",
+      "Los datos y fotografías son referenciales.",
+      "Es responsabilidad del comprador verificar que el número de serie y número de motor correspondan a la unidad.",
+      "El trámite de cambio de placas y entrega del SOAT vigente no está a cargo de la compañía, SI CORRESPONDIERA.",
+      "Todos los gastos relacionados a la logística, transporte o cualquier otro, serán pagados por el comprador y deben ser cancelados antes de la entrega del vehículo.",
+      "",
+      "El precio no incluye IGV. Debe agregar 5% de comisión al precio mostrado.",
+      
+    ].join("\n"),
+    caracteristicas: [
+      "Sistema de sonido premium",
+      "Asientos de Tela",
+      "Pantalla Digital",
+      "Aros de Serie", 
+      "Gasolina",
+      "Transmision automatica",
+      "Frenos ABS"
+    ]
+  },//58 kia taller
+
 
 ];
 
