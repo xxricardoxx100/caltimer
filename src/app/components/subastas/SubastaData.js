ad0053e9244dc42af1638d91dd12c8a2a3f0d8f1
Ricardo <xxricardoxx7779@gmail.com>
2026-08-14 10:27:55 -0500

    un carro mas

--- src/app/components/subastas/SubastaData.js
+++ src/app/components/subastas/SubastaData.js
@@ -2647,6 +2647,55 @@ export const subastaData = [
     ]
   },//59 jmc taller
 
+  {
+    id: 60,
+    marca: "Toyota",
+    modelo: "Etios",
+    año: 2023,
+    precio: 9000,
+    incremento_puja: 50, 
+    imagen: "/Autos/taller/toyota/1.png",
+    imagenes: [
+      "/Autos/taller/toyota/1.png",
+      "/Autos/taller/toyota/2.png",
+      "/Autos/taller/toyota/3.png",
+      "/Autos/taller/toyota/4.png",
+    ],  
+    kilometraje: "00,000 km",  
+    estado: "Bueno",
+    transmision: "Mecanico",
+    combustible: "Gasolina GLP",
+    color: "Plata",
+    puertas: 4,
+    // Fecha de finalización de la subasta (formato ISO 8601)
+    fecha_fin: "2026-08-14T19:50:00",
+    anexoUrl: "/anexo/TyC.pdf",
+    boletaInformativaUrl: "/Autos/taller/toyota/CAA-116.pdf",
+    descripcion:  [
+      "<strong>PLACA: CAA116</strong>",
+      "<strong>Nota importante:</strong> El comprador acepta que el vehículo podría tener papeletas, multas o cargas desconocidas por el vendedor al momento de la venta, y asume toda responsabilidad por cualquier afectación que aparezca después de la transferencia.",
+      "<strong>UBICACION: El vehiculo se encuentra en Lima.</strong>",
+      "",
+      "La venta de los vehículos se realiza en la modalidad de \"donde está y como está\".",
+      "Los datos y fotografías son referenciales.",
+      "Es responsabilidad del comprador verificar que el número de serie y número de motor correspondan a la unidad.",
+      "El trámite de cambio de placas y entrega del SOAT vigente no está a cargo de la compañía, SI CORRESPONDIERA.",
+      "Todos los gastos relacionados a la logística, transporte o cualquier otro, serán pagados por el comprador y deben ser cancelados antes de la entrega del vehículo.",
+      "",
+      "El precio no incluye IGV. Debe agregar 5% de comisión al precio mostrado.",
+      
+    ].join("\n"),
+    caracteristicas: [
+      "Sistema de sonido premium",
+      "Asientos de Tela",
+      "Pantalla Digital",
+      "Aros de Serie", 
+      "Gasolina GLP",
+      "Transmision Mecanica",
+      "Frenos ABS"
+    ]
+  },//60 TOYOTA ETIOS taller
+
 
 ];
 
